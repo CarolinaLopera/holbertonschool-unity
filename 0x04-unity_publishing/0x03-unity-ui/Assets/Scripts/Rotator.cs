d0d827551b73ff1273ae24b34ab0813ccbd4b484
CarolinaLopera <carolinalopera08@gmail.com>
2022-05-13 00:16:06 -0500

    image

---
+++ 0x04-unity_publishing/0x03-unity-ui/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update() {
+        transform.Rotate(0, 0, 45 * Time.deltaTime);
+    }
+}
